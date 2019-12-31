8f49eb63c8b6869bd8cfe20146b49996f477a12a
Jonathan Klughertz <klughertz.j@gmail.com>
2019-12-31 09:50:30 +0100

    Improve design

--- src/app/Controls/Controls.tsx
+++ src/app/Controls/Controls.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, ButtonGroup, Icon } from '@blueprintjs/core';
-import { Select } from '@blueprintjs/select';
 import { Mode } from '../../typings';
 
 import styles from './Controls.module.css';
@@ -18,37 +17,48 @@ const Grid: React.FC<IGridProps> = ({
   calculatePath,
   clearGrid,
 }) => {
-  const SizeSelect = Select.ofType<{ width: number, height: number }>();
-
   return (
     <div className={styles.root}>
-      <ButtonGroup>
-        <Button
-          text="Set Start"
-          icon={<Icon icon="stop" color="#0F9960" />}
-          active={mode === Mode.setStart}
-          onClick={() => setMode(mode === Mode.setStart ? Mode.draw : Mode.setStart)}
-          style={{ outline: 0 }}
-        />
-        <Button
-          text="Set End"
-          icon={<Icon icon="stop" color="#DB3737" />}
-          active={mode === Mode.setEnd}
-          onClick={() => setMode(mode === Mode.setEnd ? Mode.draw : Mode.setEnd)}
-          style={{ outline: 0 }}
-        />
-        <Button
-          text="Go"
-          onClick={calculatePath}
-          style={{ outline: 0 }}
-        />
-        <Button
-          text="Clear"
-          onClick={clearGrid}
-          style={{ outline: 0 }}
-        />
-      </ButtonGroup>
-    </div>
+      <div>
+        <ButtonGroup>
+          <Button
+            text="Set Start"
+            icon={<Icon icon="stop" color="#0F9960" />}
+            active={mode === Mode.setStart}
+            onClick={() => setMode(mode === Mode.setStart ? Mode.draw : Mode.setStart)}
+            style={{ outline: 0 }}
+          />
+          <Button
+            text="Set End"
+            icon={<Icon icon="stop" color="#DB3737" />}
+            active={mode === Mode.setEnd}
+            onClick={() => setMode(mode === Mode.setEnd ? Mode.draw : Mode.setEnd)}
+            style={{ outline: 0 }}
+          />
+          <Button
+            text="Draw"
+            icon={<Icon icon="stop" color="#BBBBBB" />}
+            active={mode === Mode.draw}
+            onClick={() => setMode(Mode.draw)}
+            style={{ outline: 0 }}
+          />
+        </ButtonGroup>
+        <ButtonGroup className={styles.buttonGroup}>
+          <Button
+            text="Compute Path"
+            icon={<Icon icon="build" color="#182026" iconSize={14} />}
+            onClick={calculatePath}
+            style={{ outline: 0 }}
+          />
+          <Button
+            text="Clear"
+            icon={<Icon icon="cross" color="#182026" iconSize={14} />}
+            onClick={clearGrid}
+            style={{ outline: 0 }}
+          />
+        </ButtonGroup>
+      </div>
+    </div >
   );
 };
 
